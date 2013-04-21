483f05b47c47d65bbc5dd47025a071634ef9f551
Tim <twaite@twaite-Lenovo-V570.(none)>
2013-04-21 14:53:41 -0600

    began calcAdjacencies

--- Vectrix/src/game/Board.java
+++ Vectrix/src/game/Board.java
@@ -1,6 +1,9 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 import javax.swing.JPanel;
 
@@ -9,9 +12,13 @@ public class Board extends JPanel{
 	private ArrayList<Node> nodes;
 	private Solution solution;
 	private ArrayList<Path> paths;
+	private int numRow, numCol;
+	private Map<Integer, LinkedList<Integer>> adjMtx;
 	
 	public Board() {
-		
+		nodes = new ArrayList<Node>();
+		paths = new ArrayList<Path>();
+		adjMtx = new HashMap<Integer, LinkedList<Integer>>();
 	}
 	
 	public boolean checkSolution() {
@@ -32,6 +39,19 @@ public class Board extends JPanel{
 	
 	public void generatePaths() {
 		
+	}
+	
+	public void calcAdjacencies() {
+		LinkedList<Integer> adj;
+		for ( int row = 0; row < numRow; ++row ) {
+			for ( int col = 0; col < numCol; ++col ) {
+				adj = new LinkedList<Integer>();
+			}
+		}
+	}
+	
+	public int calcIndex(int row, int col){
+		return (numCol*row) + col;
 	}
 	
 	public DrawType checkNodeDirection(Node nodeOne, Node nodeTwo) {
